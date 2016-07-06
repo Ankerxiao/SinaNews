70d8c48aedb7ce433f85677801611fdc3ba73c97
sina <807132689@qq.com>
2016-07-06 22:22:29 +0800

    123

--- SinaSH/SinaSH/Classes/PicNews/View/PCell.m
+++ SinaSH/SinaSH/Classes/PicNews/View/PCell.m
@@ -1,16 +1,16 @@
 //
-//  PicCell.m
+//  PCell.m
 //  SinaSH
 //
 //  Created by Mr Lee on 16/7/6.
 //  Copyright © 2016年 keane. All rights reserved.
 //
 
-#import "PicCell.h"
+#import "PCell.h"
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "NewsModel.h"
 
-@implementation PicCell
+@implementation PCell
 
 - (void)awakeFromNib {
     // Initialization code
@@ -21,16 +21,13 @@
 
     // Configure the view for the selected state
 }
-
 -(void)updateCellData:(NewsModel*)newsModel
 {
     [self.PicV sd_setImageWithURL:[NSURL URLWithString:newsModel.kpic]];
     [self.titleL setText:newsModel.title];
-    [self.totoalL setText:[NSString stringWithFormat:@"%@图",newsModel.pictotal]];
-    self.iocnV.image = [UIImage imageNamed:@"feed_cell_commentIcon"];
+    [self.picSumL setText:[NSString stringWithFormat:@"%@图",newsModel.pictotal]];
+    self.iconV.image = [UIImage imageNamed:@"feed_cell_commentIcon"];
     [self.commentL setText:newsModel.comment];
     
 }
-
-
 @end
